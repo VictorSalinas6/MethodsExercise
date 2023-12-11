93132d4285d536254c9416c26ab63117fef48a33
Victor Salinas <victorsalinas6@gmail.com>
2023-12-10 23:32:40 -0600

    Added the second part of the excersice

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -19,13 +19,33 @@ namespace MethodsExercise
 
             ShortStory(name, color, mascot, country, villain);
 
+            Console.WriteLine("Which 2 numbers do you wish to sum?");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The result of the sum is: {Add(num1, num2)}\n");
+
+            Console.WriteLine("Which 2 numbers do you wish to multiply?");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The result of the sum is: {Multiply(num3, num4)}\n");
+
         }
         public static void ShortStory(string name, string color, string mascot, string country, string villain)
         {
             Console.WriteLine($"Hello {name}, this is the story of the next birthday you will have, you\n" +
                 $"can decide whether you can believe it, on the day {mascot} your trusty companion\n" + 
                 $"will transform in to your worst enemy, {villain}, with a plan on changing the color\n" + 
-                $"of the sky to {color} in {country} to at last conquer the world, be prepared to stop it!");
+                $"of the sky to {color} in {country} to at last conquer the world, be prepared to stop it!\n");
+        }
+
+        public static int Add(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2) 
+        {
+            return num1 * num2;
         }
     }
 }
