f8fc02b919bca7a830f4d57a026ce730944e4bf2
Victor Salinas <victorsalinas6@gmail.com>
2023-12-10 23:09:30 -0600

    Did the excersice as requested

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please introduce your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please tell me your least favorite color");
+            string color = Console.ReadLine();
+            Console.WriteLine("Tell me the name of your mascot");
+            string mascot = Console.ReadLine();
+            Console.WriteLine("Where are you from?");
+            string country = Console.ReadLine();
+            Console.WriteLine("Tell me your least favorite person");
+            string villain = Console.ReadLine();
+
+            ShortStory(name, color, mascot, country, villain);
+
+        }
+        public static void ShortStory(string name, string color, string mascot, string country, string villain)
+        {
+            Console.WriteLine($"Hello {name}, this is the story of the next birthday you will have, you\n" +
+                $"can decide whether you can believe it, on the day {mascot} your trusty companion\n" + 
+                $"will transform in to your worst enemy, {villain}, with a plan on changing the color\n" + 
+                $"of the sky to {color} in {country} to at last conquer the world, be prepared to stop it!");
         }
     }
 }
